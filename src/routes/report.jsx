b78a133d8439cb66be894f31ebb75c67dfb3c610
Pankaj L <pankaj.ladhar@ebo.com>
2023-06-15 10:11:33 +0530

    Add tab for reporting

--- src/routes/report.jsx
+++ src/routes/report.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Tabs } from "antd";
 import withPrimaryLayout from "../layout/primary-layout";
 import { getDocuments } from "../db/firebase";
 import { Heading2 } from "../components/Heading/Heading";
@@ -14,17 +15,36 @@ const Report = () => {
     fetchData();
   }, []);
 
+  const onChange = (key) => {
+    console.log(key);
+  };
+
+  const items = [
+    {
+      key: "1",
+      label: `Active`,
+      children: <ReportTable documents={documents} />,
+    },
+    {
+      key: "2",
+      label: `Done`,
+      children: <ReportTable documents={documents} />,
+    },
+  ];
+
   return (
     <div className="container">
       {/* Need to relook */}
-      <div style={{
-        paddingTop: "80px"
-      }}>
+      <div
+        style={{
+          paddingTop: "100px",
+        }}
+      >
         <div>
           <Heading2 text={"Report"} />
         </div>
-        <div className="mb-6">
-          <ReportTable documents={documents} />
+        <div className="pt-6 mb-6">
+          <Tabs defaultActiveKey="1" items={items} onChange={onChange} />
         </div>
       </div>
     </div>
